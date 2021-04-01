89c0049ddefdecce2d9ebc2f0eeb0217a706a55b
komal swami <46648301+komalswami@users.noreply.github.com>
2021-04-01 14:58:39 +0530

    avl tree

---
+++ avl.cpp
@@ -0,0 +1,160 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class node
+{
+    public:
+        int key;
+        node *left;
+        node *right;
+        int height;
+};
+int max(int a,int b);
+int height(node *n)
+{
+    if(n==NULL)
+        return 0;
+    return n->height;
+}
+int max(int a,int b)
+{
+    return(a>b)?a:b;
+}
+node* newNode(int key)
+{
+    node* node = new node();
+    node->key = key;
+    node->left = NULL;
+    node->right = NULL;
+    node->height = 1; // new node is initially
+                      // added at leaf
+    return(node);
+}
+node *rightRotate(node *y)
+{
+    node *x = y->left;
+    node *T2 = x->right;
+ 
+    // Perform rotation
+    x->right = y;
+    y->left = T2;
+ 
+    // Update heights
+    y->height = max(height(y->left),
+                    height(y->right)) + 1;
+    x->height = max(height(x->left),
+                    height(x->right)) + 1;
+ 
+    // Return new root
+    return x;
+} 
+
+node *leftRotate(node *x)
+{
+    node *y = x->right;
+    node *T2 = y->left;
+ 
+    // Perform rotation
+    y->left = x;
+    x->right = T2;
+ 
+    // Update heights
+    x->height = max(height(x->left),   
+                    height(x->right)) + 1;
+    y->height = max(height(y->left),
+                    height(y->right)) + 1;
+ 
+    // Return new root
+    return y;
+}
+
+int getBalance(node *N)
+{
+    if (N == NULL)
+        return 0;
+    return height(N->left) - height(N->right);
+}
+
+node* insert(node* node, int key)
+{
+    /* 1. Perform the normal BST insertion */
+    if (node == NULL)
+        return(newNode(key));
+ 
+    if (key < node->key)
+        node->left = insert(node->left, key);
+    else if (key > node->key)
+        node->right = insert(node->right, key);
+    else // Equal keys are not allowed in BST
+        return node;
+ 
+    /* 2. Update height of this ancestor node */
+    node->height = 1 + max(height(node->left),
+                        height(node->right));
+ 
+    /* 3. Get the balance factor of this ancestor
+        node to check whether this node became
+        unbalanced */
+    int balance = getBalance(node);
+    if (balance > 1 && key < node->left->key)
+        return rightRotate(node);
+ 
+    // Right Right Case
+    if(balance < -1 && key > node->right->key)
+    {
+        return leftRotate(node);
+    }
+ 
+    // Left Right Case
+    if(balance > 1 && key > node->left->key)
+    {
+        node->left = leftRotate(node->left);
+        return rightRotate(node);
+    }
+ 
+    // Right Left Case
+    if(balance < -1 && key < node->right->key)
+    {
+        node->right = rightRotate(node->right);
+        return leftRotate(node);
+    }
+ 
+    /* return the (unchanged) node pointer */
+    return node;
+}
+void preOrder(node *root)
+{
+    if(root != NULL)
+    {
+        cout << root->key << " ";
+        preOrder(root->left);
+        preOrder(root->right);
+    }
+}
+
+int main()
+{
+    node *root = NULL;
+     
+    /* Constructing tree given in
+    the above figure */
+    root = insert(root, 10);
+    root = insert(root, 20);
+    root = insert(root, 30);
+    root = insert(root, 40);
+    root = insert(root, 50);
+    root = insert(root, 25);
+     
+    /* The constructed AVL Tree would be
+                30
+            / \
+            20 40
+            / \ \
+        10 25 50
+    */
+    cout << "Preorder traversal of the "
+            "constructed AVL tree is \n";
+    preOrder(root);
+     
+    return 0;
+}
