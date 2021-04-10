222b5de07a033e6243eb3d2301469a165f3cb638
komal swami <46648301+komalswami@users.noreply.github.com>
2021-04-10 23:27:57 +0530

    Create patternstar.cpp

---
+++ pattern/patternstar.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int rows=5;
+    for(int i=1;i<=5;i++)
+    {
+        for(int sp=i;sp<=rows-1;sp++)
+        {
+            cout<<" ";
+        } 
+        for(int j=1;j<=(2*i-1);j++)
+        {
+            if(j%2==0)
+            {
+        cout<<"*";
+        }
+        else
+        {
+            cout<<" ";
+        }
+        }
+        cout<<endl;
+    }
+    int space=1;
+    for(int i=rows-1;i>=1;i--)
+    {
+      
+      for(int k=1;k<=space;k++)
+      {
+          cout<<" ";
+      }
+        for(int j=1;j<=(2*i-1);j++)
+        {
+            if(j%2==0){
+                cout<<"*";
+            }
+            else
+            {
+            cout<<" ";
+            }
+        }
+        space++;
+        cout<<endl;
+}
+}
