dbf9d048b0963f5d7683fb4dfa4f756619e8b4be
komal swami <46648301+komalswami@users.noreply.github.com>
2021-04-11 20:48:28 +0530

    Create NumberPattern2.cpp

---
+++ pattern/NumberPattern2.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int row=5;
+    for(int i=1;i<=row;i++)
+    {
+        for(int sp=i;sp<=row-1;sp++)
+        {
+            cout<<" ";
+        }
+        for(int j=1;j<=(2*i-1);j++)
+        {
+            cout<<i;
+        }
+        cout<<endl;
+    }
+    return 0;
+}
